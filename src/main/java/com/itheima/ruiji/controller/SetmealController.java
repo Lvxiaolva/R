f00b2f056d57cec732514e841df104d8c4d283db
lvxiaolv <123@lvxiaolv.com>
2022-04-16 15:44:36 +0800

    redis缓存套餐

--- src/main/java/com/itheima/ruiji/controller/SetmealController.java
+++ src/main/java/com/itheima/ruiji/controller/SetmealController.java
@@ -23,7 +23,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @RestController
 @RequestMapping("/setmeal")
-@Cacheable
+
 public class SetmealController {
     @Autowired
     private SetmealService setmealService;
